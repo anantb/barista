9605efd211f392ea5f895f33890c5f21795213e8
Anant Bhardwaj <anantb@csail.mit.edu>
2014-03-24 15:50:02 -0400

    java client

---
+++ src/client/java/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+
+basepath=$(cd "$abspath/../../.."; pwd)
+
+rm -rf $abspath/bin
+rm -rf $abspath/gen-java
+
+thrift --gen java -o $abspath $basepath/src/barista.thrift
+mkdir $abspath/bin
+jar=$(cd "$abspath/lib"; pwd)
+CLASSPATH=$jar/thrift/*:$jar/slf4j/*:$jar/apache-commons/*:$abspath/bin:$abspath/gen-java
+export CLASSPATH
+javac -classpath $CLASSPATH -d $abspath/bin $abspath/src/Client.java
+
+java Client
---
+++ src/client/java/src/Client.java
@@ -0,0 +1,50 @@
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import barista.*;
+
+/** 
+ * Sample Barista Java Client
+ * 
+ * @author anantb
+ * @date 03/24/2014
+ * 
+ */
+
+public class Client {
+  public static void main(String [] args) {
+    try {
+      TTransport transport = new TSocket("localhost", 9000);
+      TProtocol protocol = new  TBinaryProtocol(transport);
+      Barista.Client client = new Barista.Client(protocol);
+
+      transport.open();
+      
+      ConnectionParams con_params = new ConnectionParams();
+      con_params.setUser("postgres");
+      con_params.setPassword("postgres");
+      con_params.setDatabase("postgres");
+
+      Connection con = client.connect(con_params);	     
+      ResultSet res = client.execute_sql(con, "SELECT 6.824 as id, 'Distributed Systems' as name", null);
+
+      for (String fielf_name : res.getField_names()) {
+        System.out.print(fielf_name + "\t");
+      }
+
+      System.out.println();
+
+      for (Tuple t : res.getTuples()) {
+        for (Cell cell : t.getCells()) {
+          System.out.print(new String(cell.getValue()) + "\t");
+        }
+        System.out.println();
+      }
+
+      transport.close();
+    } catch(Exception e) {
+      e.printStackTrace();
+    } 
+  }
+}
