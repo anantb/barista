530b64188c0d7800ca82a4a80323f3cca67b681b
Anant Bhardwaj <anantb@csail.mit.edu>
2014-03-24 04:49:08 -0400

    setup the server

--- src/server/setup.sh
+++ src/server/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 
-basepath=$(cd "$abspath/.."; pwd)
+basepath=$(cd "$abspath/../.."; pwd)
 
 rm -rf $abspath/gen-go/src
 mkdir -p $abspath/gen-go/src
 thrift --gen go -out $abspath/gen-go/src $basepath/src/barista.thrift
 GOPATH=$basepath:$abspath/gen-go
 export GOPATH
+
+go run $abspath/main/server.go $abspath/main/handler.go
