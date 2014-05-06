472387ceeaf99fc5de9da76c33fd3acd92e70338
Ubuntu <ubuntu@barista-1.(none)>
2014-05-06 17:43:30 +0000

    do setup and run

--- src/client/cpp/run.sh
+++ src/client/cpp/run.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
-
-basepath=$(cd "$abspath/../../.."; pwd)
-
-make clean
-rm -rf $abspath/bin
-rm -rf $abspath/gen-cpp
-
-thrift --gen cpp -o $abspath $basepath/src/barista.thrift
-
-make
 ./client
