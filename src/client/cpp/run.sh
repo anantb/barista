0b266e348552cfaf54aeb87fa614cd478fa8ff23
Ubuntu <ubuntu@barista-1.(none)>
2014-05-06 17:38:34 +0000

    move everything in run.sh

--- src/client/cpp/run.sh
+++ src/client/cpp/run.sh
@@ -3,7 +3,10 @@ abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 
 basepath=$(cd "$abspath/../../.."; pwd)
 
+make clean
 rm -rf $abspath/bin
 rm -rf $abspath/gen-cpp
 
 thrift --gen cpp -o $abspath $basepath/src/barista.thrift
+
+make
