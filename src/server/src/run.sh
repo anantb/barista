f195abac20e710d1a350763567396dc6fc726523
Anant Bhardwaj <anantb@csail.mit.edu>
2014-05-09 01:01:01 -0400

    setup json server

--- src/server/src/run.sh
+++ src/server/src/run.sh
@@ -19,4 +19,11 @@ if [ "$process_line" != "" ]; then
     sudo kill `echo "$process_line" | awk '{print $2}'`
 fi
 
+process_line=`sudo lsof -i :9090 | tail -1`
+if [ "$process_line" != "" ]; then
+    process_name=`echo "$process_line" | awk '{print $1}'`
+    echo "killing $process_name"
+    sudo kill `echo "$process_line" | awk '{print $2}'`
+fi
+
 (nohup go run $abspath/main/server.go $abspath/main/handler.go &) &
