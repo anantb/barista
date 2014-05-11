629c0a6bcf8765b1b53679da179ccf4a57868a4b
David G <dggoeh1@mit.edu>
2014-05-10 22:06:51 -0700

    mf

--- src/client/go/client.go
+++ src/client/go/client.go
@@ -66,7 +66,7 @@ func main() {
 
   // open connection to a machine in group 1
   con, err = clerk.OpenConnection(group_1)
-  if err != nil || con == nil{
+  if err != nil{
     fmt.Println(err)
     return
   }
