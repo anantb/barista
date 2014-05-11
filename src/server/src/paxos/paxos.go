9f5f5157721e2ee1a542e30b486cd37aa325fbd1
Anant Bhardwaj <anantb@csail.mit.edu>
2014-05-11 10:47:36 -0400

    persistant paxos

--- src/server/src/paxos/paxos.go
+++ src/server/src/paxos/paxos.go
@@ -139,7 +139,7 @@ func (px *Paxos) Prepare(args *PrepareArgs, reply *PrepareReply) error {
 
   if px.use_zookeeper {
     data, _ := px.sm.Read(px.path + "/done/" + px.peers[px.me])
-    reply.Done = strconv.Atoi(data)
+    reply.Done, _ = strconv.Atoi(data)
   } else {
     reply.Done = px.done[px.peers[px.me]]
   }
@@ -168,7 +168,7 @@ func (px *Paxos) Accept(args *AcceptArgs, reply *AcceptReply) error {
 
   if px.use_zookeeper {
     data, _ := px.sm.Read(px.path + "/done/" + px.peers[px.me])
-    reply.Done = strconv.Atoi(data)
+    reply.Done, _ = strconv.Atoi(data)
   } else {
     reply.Done = px.done[px.peers[px.me]]
   }
@@ -195,7 +195,7 @@ func (px *Paxos) Decided(args *DecidedArgs, reply *DecidedReply) error {
     data, _ := px.sm.Read(px.path + "/done/" + px.peers[px.me])
     reply.Done = strconv.Atoi(data)
   } else {
-    reply.Done = px.done[px.peers[px.me]]
+    reply.Done, _ = px.done[px.peers[px.me]]
   }
   return nil
 }
