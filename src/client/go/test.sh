31461f14ca0cb7924c6d0238ee43ed515de89e45
MV <mpvartak@gmail.com>
2014-05-10 21:59:40 -0400

    close connections in tests

--- src/client/go/test.sh
+++ src/client/go/test.sh
@@ -14,3 +14,21 @@ go test -test.run TestPartition
 go test -test.run TestUnreliable
 go test -test.run TestHole
 go test -test.run TestManyPartition
+
+
+# setup if the databases get deleted again
+
+# for x = 1 to 5 do the following
+
+# sudo mkdir /var/lib/postgresql/9.1_x
+# sudo mkdir /var/lib/postgresql/9.1_x/main
+
+# sudo chown postgres.postgres /var/lib/postgresql/9.1_x
+# sudo chown postgres.postgres /var/lib/postgresql/9.1_x/main
+
+# su - postgres
+
+# /var/lib/postgresql/9.1/bin/pg_ctl initdb -D /var/lib/postgresql/9.1_x/main -U postgres
+
+# /var/lib/postgresql/9.1/bin/pg_ctl start -D /var/lib/postgresql/9.1_x/main -U postgres -o "-p (5433 + x)"
+
