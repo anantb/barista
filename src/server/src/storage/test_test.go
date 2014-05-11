0b66bf19ce2977fdbc5bfe627de2d6fb8f7f9249
Anant Bhardwaj <anantb@csail.mit.edu>
2014-05-11 02:45:18 -0400

    storage moduke

--- src/server/src/storage/test_test.go
+++ src/server/src/storage/test_test.go
@@ -14,8 +14,7 @@ func TestBasicLeaderModified(t *testing.T) {
   sm := MakeStorageManager()
   sm.Open(servers)
   defer sm.Close()
-  sm.Create("/test/")
-  sm.Create("/test/localhost")
+  sm.Create("/test/localhost", "6.824")
   _ = sm.Write("/test/localhost", "6.824")
   data, _ := sm.Read("/test/localhost")
   if data != "6.824" {
